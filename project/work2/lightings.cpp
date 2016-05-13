3e97c7f2e8bd6ca5f230de528667f464a7ceb348
Salas <gae.m.project@gmail.com>
2016-05-13 23:17:15 +0800

    Finish three light sources(add spotlight)

--- project/work2/lightings.cpp
+++ project/work2/lightings.cpp
@@ -8,26 +8,38 @@ void set(float (&p)[4], Vector4 v)
 
 void setLightingSource()
 {
+    /** Directional Light **/
     set(world.lights[0].position, Vector4(2, 2, 2, 0));
-    set(world.lights[0].ambient, Vector4(0.1, 0.1, 0.1, 1));
-    set(world.lights[0].diffuse, Vector4(0.6, 0.6, 0.6, 1));
-    world.lights[0].constantAttenuation = 3;
+    set(world.lights[0].ambient, Vector4(0.5, 0.5, 0.5, 1));
+    set(world.lights[0].diffuse, Vector4(1, 1, 1, 1));
+    set(world.lights[0].specular, Vector4(0.2, 0.2, 0.2, 1));
+    world.lights[0].constantAttenuation = 0.8;
     world.lights[0].spotExponent = 1;
+    world.lights[0].spotCutoff = 0;
     world.lights[0].is_on = 1;
-    
-    set(world.lights[1].position, Vector4(2, 2, 2, 1));
-    set(world.lights[1].ambient, Vector4(0.3, 0.3, 0.3, 1));
-    set(world.lights[1].diffuse, Vector4(0.6, 0.6, 0.6, 1));
-    set(world.lights[1].specular, Vector4(0.6, 0.6, 0.6, 1));
-    world.lights[1].constantAttenuation = 3;
+
+    /** Point Light **/
+    set(world.lights[1].position, Vector4(-2, -2, 2, 1));
+    set(world.lights[1].ambient, Vector4(0.5, 0.5, 0.5, 1));
+    set(world.lights[1].diffuse, Vector4(1, 1, 1, 1));
+    set(world.lights[1].specular, Vector4(0.8, 0.8, 0.8, 1));
+    world.lights[1].constantAttenuation = 1;
+    world.lights[1].linearAttenuation = 0.7;
+    world.lights[1].quadraticAttenuation = 0.3;
     world.lights[1].spotExponent = 1;
+    world.lights[1].spotCutoff = 0;
     world.lights[1].is_on = 0;
 
-    set(world.lights[1].position, Vector4(2, 2, 2, 1));
-    set(world.lights[1].ambient, Vector4(0.7, 0.3, 0.3, 1));
-    set(world.lights[1].diffuse, Vector4(0.6, 0.1, 0.6, 1));
-    set(world.lights[1].specular, Vector4(0.6, 0.1, 0.6, 1));
-    world.lights[1].constantAttenuation = 5;
-    world.lights[1].spotExponent = 1;
-    world.lights[1].is_on = 0;
+    /** Spotlight **/
+    set(world.lights[2].position, Vector4(0, 0, 12, 1));
+    set(world.lights[2].ambient, Vector4(0.5, 0.5, 0.5, 1));
+    set(world.lights[2].diffuse, Vector4(1, 1, 1, 1));
+    set(world.lights[2].specular, Vector4(0.6, 0.8, 0.8, 1));
+    set(world.lights[2].spotDirection, Vector4(0, 0, -1, 0));
+    world.lights[2].constantAttenuation = 0.4;
+    world.lights[2].linearAttenuation = 0.8;
+    world.lights[2].quadraticAttenuation = 0.3;
+    world.lights[2].spotCutoff = 1;
+    world.lights[2].spotExponent = 1;
+    world.lights[2].is_on = 0;
 }
--- project/work2/shader.vert
+++ project/work2/shader.vert
@@ -14,7 +14,7 @@ struct LightSourceParameters {
     vec4 specular;
     vec4 position;
     vec4 halfVector;
-    vec3 spotDirection;
+    vec4 spotDirection;
     float spotExponent;
     float spotCutoff; // (range: [0.0,90.0], 180.0)
     float spotCosCutoff; // (range: [1.0,0.0],-1.0)
@@ -54,7 +54,7 @@ void main() {
         vec3 H = normalize(L + V);
         float attenuation = LightSource[i].constantAttenuation;
 
-        /* Is directional light */
+        /* For Point Light */
         if (LightSource[i].position.w == 1) {
             L = normalize(light_position_camera - model_position_camera);
 
@@ -64,11 +64,21 @@ void main() {
                 + LightSource[i].linearAttenuation * d
                 + LightSource[i].quadraticAttenuation * d * d
             ));
+
+            /* For Spotlight */
+            if (LightSource[i].spotCutoff != 0) {
+                vec3 v = normalize(light_position_camera - model_position_camera);
+                vec3 dir = normalize(LightSource[i].spotDirection.xyz);
+                float theta = degrees(acos(dot(v, dir)));
+                if (theta < LightSource[i].spotCutoff) {
+                    attenuation *= pow(max(dot(v, dir), 0), LightSource[i].spotExponent);
+                } else attenuation = 0;
+            }
         }
 
         vec4 ambient = Material.ambient * LightSource[i].ambient;
         vec4 diffuse = Material.diffuse * LightSource[i].diffuse * max(dot(L, N), 0);
-        vec4 specular = Material.specular * LightSource[i].specular * pow(max(dot(H, N), 0), LightSource[i].spotExponent);
+        vec4 specular = Material.specular * LightSource[i].specular * pow(max(dot(H, N), 0), 0.87);
 
         r += ambient + attenuation * (diffuse + specular);
     }
