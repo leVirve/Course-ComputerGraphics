303245cdbf8c0650e32ab072805b4f1faaa1aec2
Salas <gae.m.project@gmail.com>
2016-05-20 22:43:56 +0800

    Add line back for previous work's (work1) compability - TA once remove this line (creating default group in obj model) - cannot get a sence of why remove it !

--- project/common/glm.c
+++ project/common/glm.c
@@ -446,6 +446,7 @@ glmFirstPass(GLMmodel* model, FILE* file)
     char        buf[128];
     
     /* make a default group */
+    group = glmAddGroup(model, "default");
 
     numvertices = numnormals = numtexcoords = numtriangles = 0;
     while(fscanf(file, "%s", buf) != EOF) {
